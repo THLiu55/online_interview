418e1fd5149a106e867511bf01d59c6ff46753e5
Qi Sun <2769059069@qq.com>
2022-09-18 13:17:42 +0800

    补充email config

--- config.py
+++ config.py
@@ -8,3 +8,13 @@ DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'ewqr9urjewfoifd3'
+
+# email config
+MAIL_SERVER = "smtp.qcloudmail.com"
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_DEBUG = True
+MAIL_USERNAME = "proj.deadlineis.today"
+MAIL_PASSWORD = "MethodologyPJ2022"
+MAIL_DEFAULT_SENDER = "methodology@proj.deadlineis.today"
