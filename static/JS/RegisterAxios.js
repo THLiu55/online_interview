6c53f999396699d436b3d21be7ded5c768ba191c
Grey Xiao <zhiyou.xiao@ucdconnect.ie>
2022-09-25 20:26:42 +0800

    更改send email的axios

--- static/JS/RegisterAxios.js
+++ static/JS/RegisterAxios.js
@@ -1,17 +1,33 @@
 import axios from "axios";
 
+function sendCaptcha(){
+    var email = document.getElementById('user').value;
+
+    axios.post('/captcha', {
+        email: email
+    })
+        .then(function (response){
+            console.log(response);
+        })
+        .catch(function (error){
+            console.log(error);
+        })
+}
+
 function sendRegister(){
     var email = document.getElementById('user').value;
-    var capcha = document.getElementById('captcha').value;
+    var captcha = document.getElementById('captcha').value;
     var name = document.getElementById('username').value;
     var password = document.getElementById('pass').value;
     var re_password = document.getElementById('pass_re').value;
 
     axios.post('/register', {
-        user_email: email,
-        captcha: capcha,
-        user_name: name,
-        user_password:password
+        email: email,
+        captcha: captcha,
+        userName: name,
+        password: password,
+        repassword: re_password
+
     })
         .then(function (response) {
             console.log(response);
