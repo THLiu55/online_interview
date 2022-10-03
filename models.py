4c07608fc8263972dc56cbd0eef1d366b98bafca
Qi Sun <2769059069@qq.com>
2022-10-03 15:30:53 +0800

    更新

--- models.py
+++ models.py
@@ -17,3 +17,12 @@ class EmailCaptchaModel(db.Model):
     email = db.Column(db.CHAR(100), nullable=False, unique=True)
     captcha = db.Column(db.CHAR(10), nullable=False)
     create_time = db.Column(db.DateTime, default=datetime.now)
+
+
+class CreateInterviewModel(db.Model):
+    __tablename__ = "create_interview"
+    position = db.Column(db.CHAR(100),nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    time = db.Column(db.TIMESTAMP, nullable=False)
+    time_span = db.Column(db.Integer,nullable=False)
+    interviewee_name = db.Column(db.CHAR, nullable=False)
