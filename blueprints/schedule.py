03b4169fac9a6993ed4a6522cd1c4d0ea868287e
Qi Sun <2769059069@qq.com>
2022-11-08 22:51:40 +0800

    传输面试表给前端

--- blueprints/schedule.py
+++ blueprints/schedule.py
@@ -13,11 +13,15 @@ def register_check():
     user_email = session.get('user_email')
     print("session",session.get('user_email'))
     position = data['position']
+    print(position)
     date = data['date']
+    print(date)
     time = data['time']
+    print(time)
     time_span = data["time_span"]
+    print(time_span)
     interviewee_name = data["interviewee_name"]
-
+    print(interviewee_name)
 
     if True:
          # 构建CreateInterviewModel模型
@@ -25,3 +29,23 @@ def register_check():
         db.session.add(create_interview_model)
         db.session.commit()
         return render_template("enterroom.html")
+
+@bp.route("/total_interview", methods=['GET'])
+def get_total_interview():
+    total_interview = len(CreateInterviewModel.query.filter_by().all())
+    return jsonify({'total_interview': total_interview}), 200
+
+@bp.route("/per_interview")
+def get_per_interview():
+    per_interview = CreateInterviewModel.query.all()
+    data = []
+    for i in per_interview:
+        dict = {
+            "position": i.position,
+            "date": i.date,
+            "time": i.time,
+            "time_span": i.time_span,
+            "interviewee_name": i.interviewee_name
+        }
+        data.append(dict)
+    return jsonify({"code": 200, "data": data})
